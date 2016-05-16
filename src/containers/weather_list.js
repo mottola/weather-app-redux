e4a48aa6845e9968e653732580ea9955a4f0d4a6
Rick Mottola <mottola.rick@gmail.com>
2016-05-16 01:11:54 -0600

    added google maps feature

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 
 class WeatherList extends Component {
@@ -9,11 +10,12 @@ class WeatherList extends Component {
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => ((temp - 273.15) * 1.8) + 32);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     console.log(temps);
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color='orange' units='&#8457;' /></td>
         <td><Chart data={pressures} color='green' units='hPa'/></td>
         <td><Chart data={humidities} color='black' units='%'/></td>
@@ -26,7 +28,7 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature ('&#8457;')</th>
+            <th>Temperature (&#8457;)</th>
             <th>Pressure (hPa)</th>
             <th>Humidity (%)</th>
           </tr>
